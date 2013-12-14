5eb53245f709c38b2d7fdae0f3af90acb87a3611
刘猛 <liumeng@liumengdeMacBook-Pro.local>
2013-12-14 15:39:45 +0800

    Initial Commit

---
+++ test1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test1
+//
+//  Created by 刘猛 on 13-12-14.
+//  Copyright (c) 2013年 刘猛. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
